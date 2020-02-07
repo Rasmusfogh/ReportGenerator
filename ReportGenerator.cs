8067e09b7605d39b55275862b4a565bb4105b27e
Rasmus <201806493@post.au.dk>
2020-02-07 11:58:14 +0100

    Add project files.

---
+++ ReportGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReportGenerator
+{
+
+
+    abstract class ReportGenerator
+    {
+        public abstract void CompileReport(EmployeeDB e);
+
+        protected List<Employee> GenerateList(EmployeeDB _employeeDb)
+        {
+            var employees = new List<Employee>();
+            Employee employee;
+
+            _employeeDb.Reset();
+
+            // Get all employees
+            while ((employee = _employeeDb.GetNextEmployee()) != null)
+                employees.Add(employee);
+
+            return employees;
+        }
+    }
+
+    class NameFirst : ReportGenerator
+    {
+        public override void CompileReport(EmployeeDB em)
+        {
+            var employees = base.GenerateList(em);
+
+            foreach (var e in employees)
+            {
+                Console.WriteLine("Name-first report");
+                Console.WriteLine("------------------");
+                Console.WriteLine("Name: {0}", e.Name);
+                Console.WriteLine("Salary: {0}", e.Salary);
+                Console.WriteLine("------------------");
+            }
+        }
+    }
+
+    class SalaryFirst : ReportGenerator
+    {
+        public override void CompileReport(EmployeeDB em)
+        {
+            var employees = base.GenerateList(em);
+
+            Console.WriteLine("Salary-first report");
+
+            foreach (var e in employees)
+            {
+                Console.WriteLine("------------------");
+                Console.WriteLine("Salary: {0}", e.Salary);
+                Console.WriteLine("Name: {0}", e.Name);
+                Console.WriteLine("------------------");
+            }
+
+        }
+    }
+}
---
+++ ReportGeneratorClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ReportGenerator
+{
+    internal class ReportGeneratorClient
+    {
+        private static void Main()
+        {
+            var db = new EmployeeDB();
+
+            // Add some employees
+            db.AddEmployee(new Employee("Anne", 3000));
+            db.AddEmployee(new Employee("Berit", 2000));
+            db.AddEmployee(new Employee("Christel", 1000));
+
+
+            // Create a default (name-first) report
+            var reportGenerator = new NameFirst();
+            reportGenerator.CompileReport(db);
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            // Create a salary-first report
+            var reportGenerator2 = new SalaryFirst();
+            reportGenerator2.CompileReport(db);
+        }
+    }
+}
